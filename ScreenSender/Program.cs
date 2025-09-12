818b1e72f6b41babe5574274bb8edf96b7458695
xiaodong.wang <xiaodong.wang@hexagon.com>
2025-09-12 09:40:55 +0800

    modify somthing

--- ScreenSender/Program.cs
+++ ScreenSender/Program.cs
@@ -1,8 +1,11 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Drawing.Imaging;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Runtime.InteropServices;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace ScreenSender
